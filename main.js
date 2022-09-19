b7dc421ce13718634f0c95645082db2ff6695023
nicof_31 <nfrugoni31@gmail.com>
2022-09-19 18:55:32 -0300

    se da inicio en github

---
+++ main.js
@@ -0,0 +1,70 @@
+const MIN_POWER = 10;
+const MAX_POWER = 30;
+
+
+let energiaSuperman = 100;
+let energiaGoku = 100;
+
+
+console.log(energiaSuperman)
+console.log(energiaGoku)
+
+let round = 0;
+
+//solcita ingresar un valor para dar inicio al juego
+let inicio = prompt("Por favor ingreses tipee inicio si quiere comenzar el juego");
+
+
+if(inicio !=null){
+switch(inicio.toLowerCase()){
+        case "inicio": 
+            console.log("Tipeo: " + inicio);
+                  
+    
+    
+while (energiaGoku > 0 && energiaSuperman > 0){
+    round = round + 1
+
+let golpeGoku = Math.ceil(Math.random() * (MAX_POWER - MIN_POWER) + MIN_POWER)
+console.log("Golpe Goku " + golpeGoku)
+let golpeSuperman = Math.ceil(Math.random() * (MAX_POWER - MIN_POWER) + MIN_POWER)
+console.log("Golpe Superman " + golpeSuperman)
+
+if(golpeGoku === golpeSuperman){
+    energiaSuperman -= golpeGoku
+    energiaGoku -= golpeSuperman
+}else if(golpeSuperman > golpeGoku) {
+    document.write('<div class="card"><h5>Round N° ' + round + ': Energía Superman:  ' + energiaSuperman + ' vs ' + 'Energía Goku: '+ energiaGoku + '</h5><img src="img/golpeSuperman.jpeg" /></div>')
+
+    energiaGoku -= golpeSuperman
+} else {
+    energiaSuperman -= golpeGoku
+
+    document.write('<div class="card"><h5>Round N° ' + round + ': Energía Superman:  ' + energiaSuperman + ' vs ' + 'Energía Goku: '+ energiaGoku + '</h5><img src="img/golpeGoku.jpeg" /></div>');
+    
+}
+
+document.write('<br>');
+
+console.log("Round Nº " + round)
+console.log("Energia Superman " + energiaSuperman)
+console.log("Energia Goku " + energiaGoku)
+
+}
+default: 
+console.log("RESULTADO COMBATE:");
+}
+}
+
+if(energiaGoku === energiaSuperman){
+    console.log("Empate técnico por error de tipeo incio")
+}else if(energiaGoku>0){
+    console.log("Gano Goku")
+    document.write('<div class="ganador"><h5>Gano Goku</h5><img src="img/wingoku.jpeg" /></div>')
+    document.write('<div class="row row-cols-12 row-cols-md-4 card"><h2>Fin combate</h2></div>')
+}else {
+    console.log("Gano Superman")
+    document.write('<div class="ganador"><h5>Gano Superman</h5><img src="img/winsuperman.jpg" /></div>')
+    document.write('<div class="row row-cols-12 row-cols-md-4 card"><h2>Fin combate</h2></div>')
+}
+ 
---
+++ notas.js
@@ -0,0 +1,52 @@
+//alert()
+
+let contador = 0;
+contador = 1;
+contador = contador + 1;
+console.log(contador)
+contador = contador + 1;
+console.log(contador)
+contador += 1
+console.log(contador)
+contador +=2
+console.log(contador)
+contador -=3
+console.log(contador)
+
+/* 
+contador++
+console.log(contador)
+++contador
+ */
+
+
+const num1 = 1;
+const num2 = 1.5;
+const num3 = "2.5";
+const num4 = 0.1234;
+const num5 = -3;
+
+console.log(typeof num1)
+console.log(typeof num3)
+
+console.log(+num3);
+console.log(Number(num3));
+console.log(parseInt(num2));
+console.log(parseFloat(num3));
+
+console.log('=========================')
+let resultado;
+
+resultado = Math
+resultado = Math.PI;
+resultado = Math.round(2.49);
+resultado = Math.round(2.50);
+resultado = Math.ceil(2.1);
+resultado = Math.floor(2.9);
+
+resultado = Math.random();
+
+resultado = Math.floor(Math.random()*100)
+
+
+console.log(resultado)
